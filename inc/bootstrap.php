e516a98dc4975dfefcb146941c3f3d98d251cf6b
Wils Iglesias <wiglesias83@gmail.com>
2021-11-25 00:06:23 +0100

    Create BaseController class contains the utility methods

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-define("PROJECT_ROOT_PATH", __DIR__ . "/../");
+const PROJECT_ROOT_PATH = __DIR__ . "/../";
 
 // include main configuration file
 require_once PROJECT_ROOT_PATH  . "/inc/config.php";
