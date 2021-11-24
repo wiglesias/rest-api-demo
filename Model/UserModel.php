e2048e22d0fa02af55325960f7bbda9ada7291d1
Wils Iglesias <wiglesias83@gmail.com>
2021-11-24 23:28:39 +0100

    Initial commit. Create initial projecte structure. Add Database and UserModel classes

---
+++ Model/UserModel.php
@@ -0,0 +1,11 @@
+<?php
+
+//declare(strict_types=1);
+
+class UserModel extends Database
+{
+    public function getUsers($limit)
+    {
+        return $this->select("SELECT * FROM users ORDER BY user_id ASC LIMIT ?", ["i", $limit]);
+    }
+}
